bfcdb5ddac2dfb1e39c498b7688f74d8c66aaf5a
1727003855 <ejc.damian@yavirac.edu.ec>
2022-07-22 22:52:30 -0500

    fix bugs

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import image from "../image/fondo.jpg"
 import photo from "../image/photo.webp"
 
 function Header (){
+
     return(
         <section className="p-5"  style={{ backgroundImage: `url(${image})` }}>
             <div  className="flex justify-center flex-col">
                 <img src={photo} alt="" className="rounded-full h-45 w-45 m-auto"/>
-                <span className="text-5xl font-bold text-white m-auto leading-normal p-7 w-13">Erick Damian Developer Full Stack</span>
+                <p className="text-5xl font-bold text-white leading-normal w-12 m-auto">Erick Damian Developer Full Stack</p>
             </div>
         </section>
 
