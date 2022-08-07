597dc0693e4aae9ccb743a0fed99da8bdc20a9f4
1727003855 <ejc.damian@yavirac.edu.ec>
2022-08-07 06:17:57 -0500

    git push

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,48 @@
+import emailjs from "emailjs-com";
+import {useState} from "react";
 
 function Contact(){
+    const [formEmail, setFormEmail]=useState({});
 
+    const handleInputChange=(event)=>{
+        setFormEmail({
+            ...formEmail,
+            [event.target.name]: event.target.name
+        })
+    }
+
+    const sendEmail=(event)=>{
+        event.preventDefault()
+        emailjs.sendForm('service_0z9mg2q','template_zzfmbym',event.target,'t7zHzqKVm0A2nsGdz').then(res=>{
+            alert("El correo se envío correctamente")
+        }).catch(error=>{
+            console.log(error)
+        })
+    }
     
     return(<div>
         <div>
             <div id="contact" className="flex min-h-screen items-center justify-start bg-white">
                 <div className="mx-auto w-full max-w-lg">
                     <h1 className="text-4xl font-medium">Contáctanos</h1>
-                    <p className="mt-3">Pedes enviarme un correo a ejc.damian@yavirac.edu.ec o enviarme un mensaje por aqui:</p>
-                    <form action="https://api.web3forms.com/submit" className="mt-10">
-                        <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" />
+                    <p className="mt-3">Puedes enviarme un correo a ejc.damian@yavirac.edu.ec o enviarme un mensaje por aqui:</p>
+                    <form onSubmit={sendEmail} className="mt-10">
                         <div className="grid gap-6 sm:grid-cols-2">
                             <div className="relative z-0">
-                                <input type="text" name="name" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
-                                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your name</label>
+                                <input type="text" name="name" onChange={handleInputChange} className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
+                                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Nombre</label>
                             </div>
                             <div className="relative z-0">
-                                <input type="text" name="email" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
-                                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your email</label>
+                                <input type="text" name="email" onChange={handleInputChange} className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " />
+                                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Correo</label>
                             </div>
                             <div className="relative z-0 col-span-2">
-                                <textarea name="message" rows="5" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "></textarea>
-                                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your message</label>
+                                <textarea name="message" onChange={handleInputChange} rows="5" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "></textarea>
+                                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Mensaje</label>
+
                             </div>
                         </div>
-                        <button type="submit" className="mt-5 rounded-md bg-black px-10 py-2 text-white hover:bg-gray-700 hover:border-gray-700 hover:text-white focus-within:bg-gray-700 focus-within:border-gray-700 focus-within:text-white">Send Message</button>
+                        <button type="submit" className="mt-5 rounded-md bg-black px-10 py-2 text-white hover:bg-gray-700 hover:border-gray-700 hover:text-white focus-within:bg-gray-700 focus-within:border-gray-700 focus-within:text-white">Enviar mensaje</button>
                     </form>
                 </div>
             </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,15 +5,13 @@ function Project(){
 
     const [projectData]=useState(data);
 
-    console.log(projectData)
-
     return(
         <div id="project" className="bg-gray-100">
             <h1 className="m-0 text-4xl font-semibold leading-tight tracking-tight text-center text-black border-0 border-gray-200 sm:text-4xl">PROYECTOS</h1>
             {projectData.map(value=>{
                 return(
-                    <div className="bg-gray-100 py-8 m-auto px-4 leading-7 text-gray-900 bg-white sm:py-2 md:py-4 lg:py-10 ">
-                        <div key={value.id} className="max-w-6xl px-4 px-10 mx-auto border-solid lg:px-12">
+                    <div key={value.id} className="bg-gray-100 py-8 m-auto px-4 leading-7 text-gray-900 bg-white sm:py-2 md:py-4 lg:py-10 ">
+                        <div  className="max-w-6xl px-4 px-10 mx-auto border-solid lg:px-12">
                             <div className="flex flex-col items-start leading-7 text-gray-900 border-0 border-gray-200 lg:items-center lg:flex-row p-5  rounded-md shadow-lg shadow-gray-400/50 ">
                                 <div className="box-border flex-1 text-center border-solid sm:text-left">
                                     <h2 className="m-0 text-3xl font-semibold leading-tight tracking-tight text-left text-gray-500 border-0 border-gray-200 sm:text-3xl">
