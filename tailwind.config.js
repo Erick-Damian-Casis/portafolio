2d915325623aac4fc9c6dca354ad5af5895b13aa
1727003855 <ejc.damian@yavirac.edu.ec>
2022-12-15 23:15:40 -0500

    fix bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        forest:'#2d112b'
+      }
+    },
+    fontFamily:{
+      rubik: ['Rubik Vinyl'],
+      titan: ['Titan One'],
+      oleo: ['Oleo Script'],
+      vadodara: ['Hind Vadodara'],
+    },
   },
   plugins: [],
 }
